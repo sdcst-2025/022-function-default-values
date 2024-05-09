13e02bfd528bb98da5ecc8b78b60330fa00c7fbe
hungrybeagle <cyang@deltalearns.ca>
2024-05-09 11:36:36 -0700

    update 2024

--- task1_test.py
+++
@@ -1,9 +0,0 @@
-#!python3
-
-import task1
-
-def test1():
-    assert task1.sentence("Hello") == "Hello Benjamin. How are you"
-
-def test2():
-    assert task1.sentence("Hiya","Casey","Have you enjoyed your meal") == "Hiya Casey. Have you enjoyed your meal"
--- task3_test.py
+++
@@ -1,9 +0,0 @@
-#!python3
-
-import task3
-
-def test1():
-    assert task3.title("*") == "*********\n* Title *\n*********"
-
-def test2():
-    assert task3.title() == "=========\n= Title =\n========="
